3a22ce8dbff2de26dbc66f50fa02e0be75f3b405
E. L. Calamia <hello@ecalamia.com>
2022-12-01 00:19:53 +0100

    Do not rely on clunky time.Sleep calls anymore

--- crown.go
+++ crown.go
@@ -2,15 +2,21 @@ package crown
 
 import (
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
 // Clock represents a controllable clock. The function NewClock returns a new
 // one, so there is no need to initialize Clock directly.
 type Clock struct {
-	mu      sync.Mutex
-	tick    sync.Cond
-	current time.Time
+	mu         sync.RWMutex
+	tick       sync.Cond
+	current    time.Time
+	sleepCount int32 // For testing purposes
+}
+
+func (c *Clock) GetSleepCount() int32 {
+	return atomic.LoadInt32(&c.sleepCount)
 }
 
 // NewClock initializes and returns a new Clock object which starts at time t.
@@ -23,8 +29,8 @@ func NewClock(t time.Time) *Clock {
 
 // Now returns the current clock time.
 func (c *Clock) Now() time.Time {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 	return c.current
 }
 
@@ -33,19 +39,13 @@ func (c *Clock) Forward(d time.Duration) {
 	c.mu.Lock()
 	c.current = c.current.Add(d)
 	c.mu.Unlock()
-	c.Tick()
-}
-
-// Tick lets all listeners know that the current time has changed. Tick is
-// automatically called when Forward is called.
-func (c *Clock) Tick() {
-	time.Sleep(time.Millisecond) // rate-limit Broadcast call
 	c.tick.Broadcast()
 }
 
 // Sleep returns when the clock has reached its curent time + the specified
 // duration d.
 func (c *Clock) Sleep(d time.Duration) {
+	atomic.AddInt32(&c.sleepCount, 1)
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	deadline := c.current.Add(d)
--- crown_test.go
+++ crown_test.go
@@ -1,8 +1,6 @@
 package crown
 
 import (
-	"sync"
-	"sync/atomic"
 	"testing"
 	"time"
 )
@@ -32,27 +30,56 @@ func TestSleep(t *testing.T) {
 	refT, _ := time.Parse(time.RFC3339, "2022-11-25T01:00:00Z")
 	clock := NewClock(refT)
 
-	var returned int32 = 0
-	var wg sync.WaitGroup
-	wg.Add(1)
+	done1 := make(chan struct{})
+	done2 := make(chan struct{})
+
+	// Sleeper 1 (6 secs)
 	go func() {
-		defer wg.Done()
-		clock.Sleep(9 * time.Second)
-		atomic.AddInt32(&returned, 1)
+		clock.Sleep(6 * time.Second)
+		done1 <- struct{}{}
 	}()
 
-	clock.Tick()
-	clock.Forward(2 * time.Second)
-	clock.Forward(2 * time.Second)
-	if atomic.LoadInt32(&returned) == 1 {
-		t.Errorf("Sleep returned too early after 4 secs")
+	// Sleeper 2 (4 secs)
+	go func() {
+		clock.Sleep(4 * time.Second)
+		done2 <- struct{}{}
+	}()
+
+	// Wait for sleepers to be ready (retry=5)
+	for try := 1; ; try++ {
+		c := clock.GetSleepCount()
+		if c == 2 {
+			break
+		}
+		if c != 2 && try == 5 {
+			t.Fatalf("Retry=5 times to wait for sleepers")
+		}
+		time.Sleep(time.Millisecond)
 	}
-	clock.Forward(2 * time.Second)
-	clock.Forward(2 * time.Second)
-	clock.Forward(2 * time.Second)
-	if atomic.LoadInt32(&returned) == 0 {
-		t.Errorf("Sleep did not return after 10 secs")
-		return
+
+	clock.Forward(1 * time.Second)
+	clock.Forward(1 * time.Second) // +2 secs
+	select {
+	case <-done1:
+		t.Fatalf("Sleeper 1 returned prematuraly at t+2s. t=%q", clock.Now())
+	case <-done2:
+		t.Fatalf("Sleeper 2 returned prematuraly at t+2s. t=%q", clock.Now())
+	default:
 	}
-	wg.Wait()
+	clock.Forward(1 * time.Second)
+	clock.Forward(1 * time.Second) // +4 secs
+	select {
+	case <-done2:
+		t.Log("Sleeper 2 returned.", clock.Now())
+	case <-time.After(1 * time.Second):
+		t.Fatalf("Sleeper 2 did not return after clock has reached t+4s. t=%q", clock.Now())
+	}
+	clock.Forward(1 * time.Second)
+	clock.Forward(1 * time.Second) // +6 secs
+	select {
+	case <-done1:
+		t.Log("Sleeper 1 returned.", clock.Now())
+	case <-time.After(1 * time.Second):
+		t.Fatalf("Sleeper 1 did not return after clock has reached t+6s. t=%q", clock.Now())
+	}
 }
